37428023e0e970cc117e9f56e295a35fc69b94f4
Claire G <claire.gobert2002@gmail.com>
2024-11-21 10:24:40 +0100

    Ajout exo tri de pixels par blocs et degrade de couleur

--- src/main.cpp
+++ src/main.cpp
@@ -397,6 +397,86 @@ void pixelsSorting3(sil::Image &image)
         }
     }
 }
+// Tri par blocs
+void pixelsSorting4(sil::Image &image){
+    for (int i=0; (i + 70) < image.pixels().size(); i++)
+    {
+        if (random_int(0, 150) == 75){
+            std::sort(image.pixels().begin() + i, image.pixels().begin() + (i + 70), [](glm::vec3 const& color1, glm::vec3 const& color2) {
+                return brightness(color1) > brightness(color2);
+            });
+            i = i + 70;
+        }
+    }
+}
+
+glm::vec3 srgb_to_linear(glm::vec3 c){
+    for(int i = 0; i < 3; i++ ){
+        if (c[i] <= 0.04045){
+            c[i] = c[i] / 12.92;
+        }
+        else{
+            c[i] = pow((c[i] + 0.055) / 1.055, 2.4);
+        }
+    } 
+    return c;
+}
+glm::vec3 linear_to_oklab(glm::vec3 c) 
+{
+    c = srgb_to_linear(c);
+    float l = 0.4121656120f * c.r + 0.5362752080f * c.g + 0.0514575653f * c.b;
+    float m = 0.2118591070f * c.r + 0.6807189584f * c.g + 0.1074065790f * c.b;
+    float s = 0.0883097947f * c.r + 0.2818474174f * c.g + 0.6302613616f * c.b;
+
+    float l_ = cbrtf(l);
+    float m_ = cbrtf(m);
+    float s_ = cbrtf(s);
+
+    return {
+        0.2104542553f*l_ + 0.7936177850f*m_ - 0.0040720468f*s_,
+        1.9779984951f*l_ - 2.4285922050f*m_ + 0.4505937099f*s_,
+        0.0259040371f*l_ + 0.7827717662f*m_ - 0.8086757660f*s_,
+    };
+}
+glm::vec3 linear_to_srgb(glm::vec3 c){
+    for(int i = 0; i < 3; i++ ){
+        if (c[i] <= 0.0031308){
+            c[i] = c[i] * 12.92;
+        }
+        else{
+            c[i] = 1.055 * pow(c[i], 1.0/2.4) - 0.055; 
+        }
+    } 
+    return c;
+    
+}
+glm::vec3 oklab_to_linear(glm::vec3 c) 
+{
+    float l_ = c.r + 0.3963377774f * c.g + 0.2158037573f * c.b;
+    float m_ = c.r - 0.1055613458f * c.g - 0.0638541728f * c.b;
+    float s_ = c.r - 0.0894841775f * c.g - 1.2914855480f * c.b;
+
+    float l = l_*l_*l_;
+    float m = m_*m_*m_;
+    float s = s_*s_*s_;
+
+    return linear_to_srgb({
+        + 4.0767245293f*l - 3.3072168827f*m + 0.2307590544f*s,
+        - 1.2681437731f*l + 2.6093323231f*m - 0.3411344290f*s,
+        - 0.0041119885f*l - 0.7034763098f*m + 1.7068625689f*s,
+    });
+}
+void coloredGradient(sil::Image &image) {
+    glm::vec3 color1 = linear_to_oklab({0.2, 1, 0.7});
+    glm::vec3 color2 =  linear_to_oklab({1, 0.6, 0.3});
+
+    for (int x{0}; x < image.width(); ++x) {
+        for (int y{0}; y < image.height(); ++y) {
+            // image.pixel(x, y) = glm::mix(color1, color2, static_cast<float>(x) / static_cast<float>(image.width() - 1));
+            image.pixel(x, y) = oklab_to_linear(glm::mix(color1, color2, static_cast<float>(x) / static_cast<float>(image.width() - 1))); 
+        }
+    }
+}
 
 void test(sil::Image &image)
 {
@@ -518,16 +598,24 @@ int main()
     //     glitch(image);
     //     image.save("output/glitch.png");
     // }
-    {
+    // {
         // sil::Image image1{"images/logo.png"};
         // pixelsSorting1(image1);
         // image1.save("output/pixelsSorting1.png");
         // sil::Image image2{"images/logo.png"};
         // pixelsSorting2(image2);
         // image2.save("output/pixelsSorting2.png");
-        sil::Image image3{"images/logo.png"};
-        pixelsSorting3(image3);
-        image3.save("output/pixelsSorting3.png");
+        // sil::Image image3{"images/logo.png"};
+        // pixelsSorting3(image3);
+        // image3.save("output/pixelsSorting3.png");
+        // sil::Image image4{"images/logo.png"};
+        // pixelsSorting4(image4);
+        // image4.save("output/pixelsSorting4.png");
+    // }
+    {
+        sil::Image image{300, 200};
+        coloredGradient(image);
+        image.save("output/coloredGradient.png");
     }
     // {
     //     sil::Image image{"images/logo.png"};
